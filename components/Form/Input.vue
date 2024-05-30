8451d5f556b8dfb4e79ea55f976a2f4b2af15547
Abdul Majid <abdulmajidcse@gmail.com>
2024-05-31 00:12:42 +0600

    custom form component.

---
+++ components/Form/Input.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const model = defineModel();
+
+defineProps<{
+  id: string;
+  title?: string;
+  type?: string;
+  required?: boolean;
+  labelClass?: string;
+  inputClass?: string;
+}>();
+</script>
+
+<template>
+  <div class="mb-5">
+    <Label :for="id" :class="labelClass">
+      <slot name="title">
+        {{ title }}
+      </slot>
+    </Label>
+    <Input
+      v-model="model"
+      :type="type"
+      :class="inputClass"
+      :id="id"
+      :required="required ?? false"
+    />
+  </div>
+</template>
---
+++ components/Form/Toggle.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  id: string;
+  title?: string;
+  type?: string;
+  required?: boolean;
+  labelClass?: string;
+  inputClass?: string;
+}>();
+</script>
+
+<template>
+  <div class="flex items-start mb-5">
+    <div class="flex items-center h-5">
+      <Input
+        :type="type"
+        :class="`!w-4 !h-4 !text-blue-600 !dark:text-blue-900 ${inputClass}`"
+        :id="id"
+        :required="required ?? false"
+      />
+    </div>
+
+    <Label :for="id" :class="`ms-2 ${labelClass}`">
+      <slot>
+        {{ title }}
+      </slot>
+    </Label>
+  </div>
+</template>
